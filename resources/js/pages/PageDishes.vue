2755c1d161796dc13e389ad33962e11f8d8aaeda
PicGigi <junior.1997@live.it>
2023-03-02 15:18:52 +0100

    Prova Luigi

--- resources/js/pages/PageDishes.vue
+++ resources/js/pages/PageDishes.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="container">
-      <h1>Sono la pagina dei piatti specifici in vue</h1>
-        <div v-for="dish in arrDishes.dishes" :key="dish.id">
-            <img :src="'storage/' + dish.image" alt="image" onerror="this.src='storage/placeholder.jpeg';">
-           <h3>{{ dish.name }}</h3>
-           <p>&euro; {{ dish.price }}</p>
-           <button class="btn btn-success" @click="$emit('addCart', dish)"> Aggiungi al carrello</button>
+        <!-- COPERTINA -->
+        <div class="">
+        </div>
+        <!-- LOGO -->
+        <div>
+            <img :src="'storage/' + user.logo_image" alt="logo" onerror="this.src='storage/placeholder.jpeg';">
+        </div>
+        <div class="cards d-flex flex-wrap">
+            <div class="rounded-4 mb-4 me-4 mycard" style="width: 15rem; height: 22rem;"
+            v-for="dish in arrDishes.dishes" :key="dish.id">
+                <img :src="'storage/' + dish.image" alt="image" onerror="this.src='storage/placeholder.jpeg';"
+                class="rounded-4" style="width: 102%; height: 50%; position: relative; left: -2px; top: -2px;">
+                <h4 class="tetx-center mt-2">{{ dish.name }}</h4>
+                <h5>&euro; {{ dish.price }}</h5>
+                <button class="m-3 rounded-5 text-white px-4 mycard-button" @click="$emit('addCart', dish)"> Aggiungi al carrello</button>
+            </div>
         </div>
    </div>
 </template>
