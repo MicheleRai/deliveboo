14b617adad42ff7ed370291ac11d15edad1777b9
Kenta Bilotta <kentabilotta20@gmail.com>
2023-03-03 18:18:59 +0100

    admin order style

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -2,29 +2,37 @@
 
 @section('content')
 <div class="container">
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Numero ordine</th>
-                <th scope="col">Indirizzo</th>
-                <th scope="col">Orario</th>
-                <th scope="col">Numero piatti ordinati</th>
-                <th scope="col">Prezzo totale</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($orders as $order)
-            <tr>
-                <th scope="row">{{ $order->order_id }}</th>
-                <th scope="row">{{ $order->address }}</th>
-                <th scope="row">{{ $order->created_at }}</th>
-                <th scope="row">{{ $order->numero_piatti }}</th>
-                <th scope="row">&euro;{{ $order->tot_price }}</th>
-                    <td>
-                        <a href="{{ route('admin.orders.show', ['order' => $order->order_id ])}}" class="btn btn-primary">Dettagli ordine</a>
-                    </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="row justify-content-center">
+        @foreach ($orders as $order)
+        <div class="order col-10 rounded-4 m-3 p-3">
+            <div class="row">
+                <div class="col-3 text-center">
+                    <h4>N. ordine</h4>
+                    <div>{{ $order->order_id }}</div>
+                </div>
+                <div class="col-3 text-center">
+                    <h4>Indirizzo</h4>
+                    <div>{{ $order->address }}</div>
+                </div>
+                <div class="col-3 text-center">
+                    <h4>Orario di Conferma</h4>
+                    <div>{{ $order->created_at }}</div>
+                </div>
+                <div class="col-3 text-center">
+                    <h4>N. piatti</h4>
+                    <div>{{ $order->numero_piatti }}</div>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-3">
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <a href="{{ route('admin.orders.show', ['order' => $order->order_id ])}}" class="order-button">Dettagli ordine</a>
+                </div>
+                <div class="col-3 d-flex align-items-center justify-content-center">
+                    <h4>Tot Ordine: <span class="euro">&euro; {{ $order->tot_price }}</span></h4>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
