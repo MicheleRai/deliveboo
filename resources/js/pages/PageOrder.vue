34b94146796dc53958167c8606caf1f8d756e137
AndreaMatarrese <matarreseandrea47@gmail.com>
2023-03-05 18:53:38 +0100

    page order

--- resources/js/pages/PageOrder.vue
+++ resources/js/pages/PageOrder.vue
@@ -1,40 +1,60 @@
 <template>
-    <div class="container">
-        <div v-for="(objCart, index) in arrCart" :key="index" class="col-5 m-auto">
-            <button class="btn btn-danger col-1" @click="deleteObj(index)">X</button>
-            {{ objCart.name }} - {{ objCart.price }}&euro;
+    <div class="container py-5 my-5">
+        <div v-if="arrCart != 0">
+            <h1 class="text-md-start text-center">Riepilogo ordine</h1>
+            <div class="col-md-12 col-11 m-auto d-flex flex-column align-items-end">
+                <div v-for="(objCart, index) in arrCart" :key="index" class="ord my-3 flex-wrap col-md-12 p-3 d-flex align-items-center justify-content-between">
+                    <div class="col-12 col-md-5 py-3">
+                        <button class="btn col-2 me-5 elimina text-light" @click="deleteObj(index)">X</button>
+                        {{ objCart.name }}
+                    </div>
+                    <div class="col-12 col-md-5 text-center py-3 d-flex align-items-center justify-content-between">
+                        <img src="'objCart.image'" alt="img" class="me-5">
+                        {{ objCart.price }}&euro;
+                    </div>
+                </div>
+                <div v-show="tot_price" class="ord my-3 col-md-2 p-3 d-flex align-items-center justify-content-end">
+                    <div class="col">
+                        Totale
+                    </div>
+                    <div class="col text-end">
+                        {{ tot_price }}&euro;
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <form @submit.prevent="submit" id="payment-form" style="margin-top: 100px;">
-            <div class="form-group">
-                <label for="name_user">Nome e cognome</label>
+        <h2 v-else class="text-md-start text-center">Non ci sono ancora ordini</h2>
+        <form @submit.prevent="submit" id="payment-form" style="margin-top: 2rem;" class="ord col-md-12 col-11 m-auto data py-5 d-flex flex-wrap justify-content-center">
+            <div class="form-group mt-3 col-10 col-md-5">
+                <label class="text-light" for="name_user">Nome e cognome</label>
                 <input type="text" class="form-control" name="name_user" id="name_user" v-model="fields.name_user" />
                 <div v-if="errors && errors.name_user" class="text-danger">{{ errors.name_user[0] }}</div>
             </div>
 
-            <div class="form-group">
-                <label for="email_user">E-mail</label>
-                <input type="email" class="form-control" name="email_user" id="email_user" v-model="fields.email_user" />
-                <div v-if="errors && errors.email_user" class="text-danger">{{ errors.email_user[0] }}</div>
-            </div>
-
-            <div class="form-group">
-                <label for="address">Indirizzo</label>
+            <div class="form-group my-3 col-10 col-md-5 ms-md-5">
+                <label class="text-light" for="address">Indirizzo</label>
                 <input type="text" class="form-control" name="address" id="address" v-model="fields.address" />
                 <div v-if="errors && errors.address" class="text-danger">{{ errors.address[0] }}</div>
             </div>
-
-            <div class="form-group">
-                <label for="note">Note</label>
+             <div class="form-group mt-3 col-10 col-md-5">
+                <label class="text-light" for="note">Note</label>
                 <textarea class="form-control" name="note" id="note" rows="5" v-model="fields.note"></textarea>
             </div>
-
-            <div v-show="tot_price">Totale: {{ tot_price }}&euro;
+            <div class="form-group mt-3 col-10 col-md-5 ms-md-5">
+                <label class="text-light" for="email_user">E-mail</label>
+                <input type="email" class="form-control" name="email_user" id="email_user" v-model="fields.email_user" />
+                <div v-if="errors && errors.email_user" class="text-danger">{{ errors.email_user[0] }}</div>
+                <button type="submit" class="btn btn-warning mt-5" :disabled="arrCart.length == 0">Procedi con l'ordine</button>
             </div>
+
+
+
+
+
             <div id="bt-dropin">
             </div>
             <input id="nonce" type="hidden" />
-            <button type="submit" class="btn btn-primary" :disabled="arrCart.length == 0">Procedi con l'ordine</button>
+
             <div v-if="success" class="alert alert-success mt-3">
                 Ordine ricevuto!
             </div>
@@ -142,4 +162,24 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.ord{
+    border-radius: 10px;
+    box-shadow: 10px 10px 16px 0px #ededed;
+    font-weight: 700;
+    font-family: 'Montserrat', sans-serif;
+    border: 1px solid #42AAF5;
+}
+.data{
+    background-color: #42AAF5;
+}
+.elimina{
+    background-color: #07CCBB;
+    box-shadow: 10px 10px 16px 0px #ededed;
+}
+.elimina:hover{
+    background-color: #dadada;
+    color: #07CCBB;
+    box-shadow: 10px 10px 16px 0px #ededed;
+}
+</style>
